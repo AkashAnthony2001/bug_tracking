ae7c5cf43febe55f265ade09fb55547f6b648cf3
Pugazhendhi GM <pugazhendhi.gm@meyicloud.com>
2023-09-14 16:25:28 +0530

    updates

--- src/pages/EditModule.jsx
+++ src/pages/EditModule.jsx
@@ -34,12 +34,6 @@ function EditModule({ moduledata, load, open, setOpen }) {
     } else {
       setmoduleNameError({ error: false, message: "" });
     }
-    // if (!editedModuleDes) {
-    //   setmoduleDesError({ error: true, message: "Module Description is required" });
-    //   return;
-    // }else {
-    //   setmoduleDesError({ error: false, message: "" });
-    // }
 
     let res = await apiService.editModuledata(moduledata._id, moduleObj);
     setEditErr(res);
@@ -81,9 +75,6 @@ function EditModule({ moduledata, load, open, setOpen }) {
             fullWidth
             variant="standard"
             value={editedModuleDes}
-            // error={moduleDesError.error}
-            // helperText={moduleDesError.error ? moduleDesError.message : ""}
-            // onChange={(e) => setEditedModuleDes(e.target.value)}
           />
           <DialogActions>
             <Button size="small" onClick={handleClose}>
